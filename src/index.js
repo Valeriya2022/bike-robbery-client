0b43ca73eb186bfb5ad979ea54431b2b9dd10f43
Valeriya2022 <49557453+Valeriya2022@users.noreply.github.com>
2021-06-15 11:14:36 +0600

    configured webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("Hello world! ");
+import './user.css';
+
+
+console.log("Hi world! ");
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,40 @@
-module.exports = {
+const path = require('path');
 
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }, 
+    module: {
+        rules: [
+            {
+                test: /\.js$/, 
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCSSExtractPlugin.loader, 'css-loader'],
+
+            }
+        ],
+        
+    },
+    plugins: [
+        new MiniCSSExtractPlugin({
+            filename: 'styles.css',
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin({}),
+    ],
 };
